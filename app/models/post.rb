ec56af3dc05f05f8c33147fdcaa02967996e92b1
Doug Kintop <dkintop@hotmail.com>
2019-11-14 11:47:00 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,23 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+  def categories_attributes=(category_attributes)
+  category_attributes.values.each do |category_attribute|
+    category = Category.find_or_create_by(category_attribute)
+    self.post_categories.build(category: category)
+    end
+  end
 
 
+  def unique_users
+    users_array= []
+    self.users.each do |user|
+      if !users_array.include?(user)
+        users_array << (user)
+      end
+      users_array 
+      #binding.pry
+    end
+  end
 end
