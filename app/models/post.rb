30adeed4ea41821b55e84e7b4ab2769ea3e28047
clonex10100 <clonex10100@gmail.com>
2019-12-05 17:00:45 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+  def categories_attributes=(attributes)
+    attributes.each do |i, attribute|
+      unless attribute[:name].empty?
+        category = Category.find_or_create_by(attribute)
+        self.categories << category
+      end
+    end
+  end
+
+
 
 end
